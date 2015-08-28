d406769bf7693cad24c5d4566df7a90d524a70be
dmalament <david.malament@levvel.io>
2015-08-28 15:35:57 -0400

    Finished part 3

--- client/app/scripts/controllers/signup.js
+++ client/app/scripts/controllers/signup.js
@@ -55,15 +55,12 @@ angular.module('clientApp')
 
     // we'll come back to here and fill in more when ready
     request.success(function (data) {
-    // our json response is recognized as
-    // the data parameter here. See? Our msg
-    // value is right there!
-      console.log(data.msg);
+        console.log(data); // <-- changed
     });
 
     request.error(function (data) {
-        console.log(data.msg);
-    });
+        console.log(data); // <-- changed
+    })
   };
 
 });
